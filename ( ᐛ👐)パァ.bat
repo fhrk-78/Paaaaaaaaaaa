526b8a864277f2e9da0a07321a0861504611a942
RubyCode50 <120289597+forestrharumaki@users.noreply.github.com>
2023-02-12 13:56:47 +0900

    a

--- ( ᐛ👐)パァ.bat
+++ ( ᐛ👐)パァ.bat
@@ -1,4 +1,4 @@
-SET /P ANSWER="何が起こったって責任はとれへんで (Y/N)"
+SET /P ANSWER="Is it really okay to run it? I warned you already. (Y/N)"
 
 if /i {%ANSWER%}=={y} (goto :jikko)
 if /i {%ANSWER%}=={yes} (goto :jikko)
@@ -6,12 +6,16 @@ if /i {%ANSWER%}=={yes} (goto :jikko)
 EXIT
 
 :jikko
+SET ahya
 @echo off
 color 40
 :zutto
+set ahya=%ahya%+1
 echo Paa
+md Paa
 type null > paa.txt
 START C:\Windows\system32\calc.exe
 START C:\Windows\system32\notepad.exe
 START https://www.google.com/search?q=paa
+mkdir
 goto :zutto
